ccd85bdff5c41f933cda3bf06c5e091dcf86b85c
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2021-01-10 00:27:46 -0500

    updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 class Contact extends Component {
     constructor(props){
@@ -13,9 +15,18 @@ class Contact extends Component {
         return(
             <div className= 'Contact'>
              <h1>You can reach me</h1>
+             <div className='logo'>
+                        <div className='gitubLogo'>
+                            <a className='logo' href='https://github.com/officialkingwil24' target='blank'>{<FontAwesomeIcon icon={faGithub} size='lg' />}</a>
+                        </div>
+                        <div className='gitubLogo'>
+                            <a className='logo' href='https://www.linkedin.com/in/wilson-pierre-louis-9665531b6/' target='blank'>{<FontAwesomeIcon icon={faLinkedin} size='lg' />}</a>
+                        </div>
 
-             <h2>Email:</h2>
-             
+                        <a href="mailto:wilsonpierrrlouis14@gmail.com">
+                            <h1> Email:wilsonpierrrlouis14@gmail.com</h1>
+                        </a>
+                    </div>
             </div>
         
         )
